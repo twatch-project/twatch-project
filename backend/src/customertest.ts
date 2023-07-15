78eaab1bcbfe4522ea3e429fbc7a0635e2eafa4b
rukchanok krumsamer <pepoil07@hotmail.com>
2023-07-15 16:16:29 +0700

    Add option in dateofbirth prisma

--- backend/src/customertest.ts
+++ backend/src/customertest.ts
@@ -4,31 +4,34 @@ const prisma = new PrismaClient();
 async function main() {
     // const createise = await prisma.user.create({
     //     data: {
-    //         username: "abc444",
+    //         username: "abv88",
     //         password: "111",
     //         role: "CUSTOMER",
     //         email: "EE@Hotmail.com",
     //     },
     // });
+    // const dateOfBirth = new Date(1982, 0, 2);
+    // const createCus = await prisma.customer.create({
+    //     data: {
+    //         firstname: "Thu",
+    //         lastname: "Hear",
+    //         gender: "MALE",
+    //         dateOfBirth: dateOfBirth,
+    //         citizenId: "12345678912554",
+    //         province: "Conburi",
+    //         district: "Ioszl",
+    //         sub_district: "556",
+    //         address: "dsdqwe",
+    //         contact: "line cosa",
+    //         userId: "4179314d-d861-48e1-8828-b14cbef57c43",
+    //     },
+    // });
 
-    const createCus = await prisma.customer.create({
-        data: {
-            firstname: "Thu",
-            lastname: "Hear",
-            gender: "MALE",
-            dateOfBirth: "11/11/1975",
-            citizenId: "1234567891234",
-            province: "Conburi",
-            district: "Ioszl",
-            sub_district: "556",
-            address: "dsdqwe",
-            contact: "line cosa",
-            userId: "8c6910a3-c031-40e2-86f2-df8de65b0d40",
-        },
-    });
-
+    const findAll = await prisma.customer.findMany({});
     // console.log(createise);
-    console.table(createCus);
+    // console.log(dateOfBirth);
+    // console.table(createCus);
+    console.table(findAll);
 }
 
 main()
