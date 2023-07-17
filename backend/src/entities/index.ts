4a72f920fd69f2cf79478795b3ed6a77d0998b5a
Chainarate <jn.chainarate@gmail.com>
2023-07-17 15:36:22 +0700

    add interface of company

--- backend/src/entities/index.ts
+++ backend/src/entities/index.ts
@@ -1,3 +1,16 @@
+// User
+
+enum Role {
+  CUSTOMER = "CUSTOMER",
+  COMPANY = "COMPANY",
+}
+
+export type UserRole = keyof typeof Role;
+
+export function mapRole(role: Role): UserRole {
+  return role;
+}
+
 export interface ICreateUser {
   username: string;
   password: string;
@@ -10,17 +23,70 @@ export interface IUser extends ICreateUser {
   userId: string;
 }
 
-export interface ICustomer {}
+// Company
 
-enum Role {
-  CUSTOMER = "CUSTOMER",
-  COMPANY = "COMPANY",
+enum Tag {
+  MINIMALMODERN = "MINIMALMODERN",
+  CONTEMPORARYMODERN = "CONTEMPORARYMODERN",
+  MODERNLUXURY = "MODERNLUXURY",
+  MODERNSTYLE = "MODERNSTYLE",
+  MIDCENTURYMODERN = "MIDCENTURYMODERN",
+  VINTAGESTYLE = "VINTAGESTYLE",
+  LOFTINDUSTRALSTYLE = "LOFTINDUSTRALSTYLE",
+  SCANDINAVIANSTYLE = "SCANDINAVIANSTYLE",
+  ARTDECO = "ARTDECO",
+  MIXANDMATCH = "MIXANDMATCH",
 }
 
-// console.log(typeof Role);
+export type HomeTag = keyof typeof Tag;
 
-export type UserRole = keyof typeof Role;
+export interface ICreateCompany {
+  companyName: string;
+  companyRegistration: string;
+  address: string;
+  sub_district: string;
+  district: string;
+  province: string;
+  postCode: number;
+  contact: string;
+  tag: HomeTag;
 
-export function mapRole(role: Role): UserRole {
-  return role;
+  userId: string;
 }
+
+export interface ICompany extends ICreateCompany {
+  companyId: number;
+}
+
+export interface ICreatePort {
+  title: string;
+  body: string;
+  tag: HomeTag;
+  address: string;
+  sub_district: string;
+  district: string;
+  province: string;
+  postCode: number;
+  createAt: Date;
+  updateAt: Date;
+
+  companyId: number;
+}
+
+export interface IPort extends ICreatePort {
+  portId: number;
+}
+
+// portId        Int @id @default(autoincrement())
+// title         String
+// body          String
+// tag           Tag
+// province      String
+// district      String
+// sub_district  String
+// address       String
+// createAt      DateTime
+// updateAt      DateTime
+
+// company     Company @relation(fields: [companyId],references: [companyId])
+// companyId   Int
