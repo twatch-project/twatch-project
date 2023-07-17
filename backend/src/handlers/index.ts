209301b71e8b2fcf3ab888ec2bd36b2841044213
Chainarate <jn.chainarate@gmail.com>
2023-07-17 15:33:52 +0700

    add interface IHandlerCompany

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { JwtAuthRequest } from "../auth";
-import { UserRole } from "../entities";
+import { HomeTag as HomeTag, UserRole } from "../entities";
 
 export interface AppRequest<Params, Body> extends Request<Params, any, Body> {}
 
@@ -23,3 +23,60 @@ export interface IHandlerUser {
   register(req: AppRequest<Empty, WithUser>, res: Response): Promise<Response>;
   getId(req: JwtAuthRequest<Empty, Empty>, res: Response): Promise<Response>;
 }
+
+// Company
+export interface WithCompany {
+  companyId: number;
+  companyName: string;
+  companyRegistration: string;
+  address: string;
+  sub_district: string;
+  district: string;
+  province: string;
+  postCode: number;
+  contact: string;
+  tag?: HomeTag;
+  userId: string;
+}
+
+export interface WithCompanyId {
+  companyId: number;
+}
+
+export interface IHandlerCompany {
+  createCompany(
+    req: JwtAuthRequest<Request, WithCompany>,
+    res: Response
+  ): Promise<Response>;
+  getCompanys(_, res: Response): Promise<Response>;
+  getCompanyById(
+    req: JwtAuthRequest<WithCompanyId, WithCompany>,
+    res: Response
+  ): Promise<Response>;
+  updateCompanyInfo(
+    req: JwtAuthRequest<WithCompanyId, WithCompany>,
+    res: Response
+  ): Promise<Response>;
+}
+
+export interface WithPort {
+  title: string;
+  body: string;
+  tag: HomeTag;
+  address: string;
+  sub_district: string;
+  district: string;
+  province: string;
+  postCode: number;
+  companyId: number;
+}
+
+export interface WithPortId {
+  portId: number;
+}
+
+export interface WithPortAndCompanyId extends WithCompanyId {
+  portId: number;
+}
+
+export interface IHandlerPorfolio {}
