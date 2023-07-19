17324c370d81c31058aabd87641f0710e13886d3
Chainarate <jn.chainarate@gmail.com>
2023-07-19 19:34:03 +0700

    create hook company and companyList

---
+++ frontend/src/hooks/useCompanyList.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { CompanyListHook } from '../types/companyList.hook'
+import { host } from '../constant'
+import { CompanyDto } from '../types/dto'
+
+export const useCompanyList = (): CompanyListHook => {
+  const [data, setData] = useState<CompanyDto[] | null>(null)
+  const [error, setError] = useState<null | unknown>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const res = await fetch(`${host}/content`)
+        const data = await res.json()
+
+        setData(data.data)
+      } catch (err: any) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return {
+    data,
+    status: {
+      error,
+      loading,
+      ready: error == null && !loading && data != null,
+    },
+  }
+}
