1d270cc47fbd12e6f9bca8717dbdabc97db8be44
Chainarate <jn.chainarate@gmail.com>
2023-07-19 19:50:05 +0700

    create guards

---
+++ frontend/src/guards/withGuard.tsx
@@ -0,0 +1,12 @@
+import { ComponentType } from 'react'
+import { useAuth } from '../providers/AuthProvider'
+import { Navigate } from 'react-router-dom'
+
+function withGuard(Component: ComponentType): ComponentType {
+  return function ComponentWithGuard(props) {
+    const { isLoggedIn } = useAuth()
+
+    if (!isLoggedIn) return <Navigate to="/login" />
+    return <Component {...props} />
+  }
+}
