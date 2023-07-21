da50b7a16b9430654275ae011a674f73254511a1
rukchanok krumsamer <pepoil07@hotmail.com>
2023-07-21 14:52:03 +0700

    Change .then to try catch part login

--- backend/src/handlers/user.ts
+++ backend/src/handlers/user.ts
@@ -55,18 +55,13 @@ class HandlerUser implements IHandlerUser {
     if (!req.payload.id) {
       return res.status(400).json({ error: "wrong username or password" });
     }
-
-    console.log(req.payload.id);
-
-    return this.repo
-      .getId(req.payload.id)
-      .then((user) => res.status(200).json(user))
-      .catch((err) => {
-        const errMsg = `failed to get id`;
-        console.error(`${errMsg}: ${err}`);
-
-        return res.status(500).json({ error: `failed to get id` });
-      });
+    try {
+      const user = await this.repo.getId(req.payload.id);
+      return res.status(200).json({ user, status: "ok" }).end();
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: `failed to get id` }).end();
+    }
   }
 
   async login(
@@ -79,44 +74,41 @@ class HandlerUser implements IHandlerUser {
         .status(400)
         .json({ error: "missing username or password in body" });
     }
-
-    return this.repo
-      .getUserByUsername(username)
-      .then((user) => {
-        if (!user) {
-          return res
-            .status(404)
-            .json({ error: `no such user: ${username}`, statusCode: 401 });
-        }
-
-        if (!compareHash(password, user.password)) {
-          return res.status(401).json({ error: `invalid credentail` });
-        }
-
-        const payload: Payload = {
-          id: user.userId,
-          username: user.username,
-          role: user.role,
-        };
-        const token = newJwt(payload);
-
-        return res.status(200).json({
+    try {
+      const user = await this.repo.getUserByUsername(username);
+      if (!user) {
+        return res
+          .status(404)
+          .json({ error: `no such user: ${username}`, statusCode: 404 })
+          .end();
+      }
+      if (!compareHash(password, user.password)) {
+        return res.status(401).json({ error: `invalid credentail` }).end();
+      }
+      const payload: Payload = {
+        id: user.userId,
+        username: user.username,
+        role: user.role,
+      };
+      const token = newJwt(payload);
+      return res
+        .status(200)
+        .json({
           status: "logined",
           id: user.userId,
           username: user.username,
           role: user.role,
           registeredAt: user.registeredAt,
           accessToken: token,
-        });
-      })
-      .catch((err) => {
-        const errMsg = `failed to get user ${username}`;
-        console.error(`${errMsg}: ${err}`);
-
-        return res
-          .status(500)
-          .json({ error: `failed to login user ${username}` });
-      });
+        })
+        .end();
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: `failed to get user with error code ${err}` })
+        .end();
+    }
   }
 
   async logout(
