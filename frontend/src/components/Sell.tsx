076410b725d9854039a49ea3604b45392f1b6356
TAICHI01001 <anothai.0978452316@gmail.com>
2023-07-24 10:57:44 +0700

    Create sell component

---
+++ frontend/src/components/Sell.tsx
@@ -0,0 +1,36 @@
+export default function Sell() {
+  return (
+    <section className="flex justify-center items-start min-h-[50vh]">
+      <div className="flex justify-center ">
+        <section className="flex flex-col justify-between  items-center my-5 p-[15px] m-auto">
+          <div className="top font-bold text-2xl p-[10px] m-[10px]">
+            <h1>Twatch No. 1 of Job Search</h1>
+          </div>
+
+          <div className="flex footer ">
+            <div className="flex flex-col h-20 text-center box1">
+              <i className="my-3">icon</i>
+              <h3 className="font-bold">Easy Search</h3>
+              <p className="p-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+            <div className="flex flex-col h-20 text-center box1">
+              <i className="my-3  ">icon</i>
+              <h3 className="font-bold">Easy Search</h3>
+              <p className="p-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+            <div className="flex flex-col h-20 text-center box1">
+              <i className="my-3 ">icon</i>
+              <h3 className="font-bold">Easy Search</h3>
+              <p className="p-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+            <div className="flex flex-col h-20 text-center box1">
+              <i className="my-3 ">icon</i>
+              <h3 className="font-bold">Easy Search</h3>
+              <p className="p-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+  )
+}
