3da5e054e5461f4421abd5d57abd0761f2151aa9
TAICHI01001 <anothai.0978452316@gmail.com>
2023-07-24 10:56:28 +0700

    create component footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className=" h-[254px]">
+      <div className="footer  text-center">
+        <hr />
+        <h1 className="font-bold p-[50px]">LOGO</h1>
+        <p className="p-[25px]">© 2023 NAMEPROJECT Inc. All rights reserved.</p>
+        <p>Privacy policy | Term of service | Contract</p>
+      </div>
+    </footer>
+  )
+}
