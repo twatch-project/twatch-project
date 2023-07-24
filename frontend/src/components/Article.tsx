20bc4bd008102ee7fe30b83a21db327627878898
TAICHI01001 <anothai.0978452316@gmail.com>
2023-07-24 10:59:06 +0700

    create component article

---
+++ frontend/src/components/Article.tsx
@@ -0,0 +1,25 @@
+export default function article() {
+  return (
+    <section className="flex justify-center items-center min-h-[50vh]">
+      <section className="flex justify-center m-[20px]">
+        <article className="flex flex-col justify-center items-center w-[1070px]">
+          <h1 className="p-[20px] font-bold text-[18px]">Lorem, ipsum dolor.</h1>
+          <p className="p-[30px]">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique distinctio cupiditate qui! Esse aperiam
+            nisi dolorum recusandae quaerat optio, saepe animi possimus ea commodi consectetur omnis explicabo tempore?
+            Nesciunt, ullam consequatur dicta aut alias recusandae. Distinctio similique quam, consequatur neque labore
+            unde eius quas asperiores. Laborum necessitatibus consequuntur adipisci vel? Ipsum facilis eveniet ducimus
+            adipisci asperiores culpa porro architecto voluptatem commodi nesciunt animi distinctio expedita, quod eum
+            optio. Aperiam reprehenderit natus eligendi, voluptate veniam necessitatibus quae quod quos hic ab mollitia
+            explicabo ipsa. Labore aliquid, natus sapiente ipsum facere culpa eius itaque quam molestiae vitae fuga,
+            magnam quos ex totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates excepturi minus
+            quidem harum id voluptas, quibusdam praesentium eaque exercitationem aspernatur itaque, facilis ullam quo.
+            Aperiam, minus. Possimus aut tempore error maxime, perferendis consectetur, expedita tempora voluptatum, sed
+            vero natus doloremque omnis dolore mollitia ratione vitae consequatur eius ipsam reprehenderit autem.
+          </p>
+          <button className="  font-bold">Show more</button>
+        </article>
+      </section>
+    </section>
+  )
+}
