3894b4f926d05c10fbb07fba68d12bb53b449526
TAICHI01001 <anothai.0978452316@gmail.com>
2023-07-24 15:04:07 +0700

    edit and add SVG

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,41 @@
 import { Toaster } from 'react-hot-toast'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Comment from './pages/Comment'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Home from './pages/Home'
+import CompanyDetail from './pages/CompanyDetail'
+import CompanyProfile from './pages/CompanyProfile'
+import CreateProfile from './pages/CreateProfile'
+import AllCompany from './pages/AllCompany'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Toaster position="top-center" reverseOrder={false} />
       {/* <Layout> */}
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <>Hello world</>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/CompanyDetail" element={<CompanyDetail />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Comment" element={<Comment />} />
+          <Route path="/CompanyProfile" element={<CompanyProfile />} />
+          <Route path="/CreateProfile" element={<CreateProfile />} />
+          <Route path="/AllCompany" element={<AllCompany />} />
+        </Route>
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
         {/* <Route path="/new" element={<Create />} /> */}
         {/* <Route path="/content/:id" element={<Content />} /> */}
         {/* <Route path="/content/:id/edit" element={<Edit />} /> */}
         {/* <Route path="*" element={<Error message="Page Not Found" />} /> */}
       </Routes>
       {/* </Layout> */}
-    </div>
+    </>
   )
 }
 
