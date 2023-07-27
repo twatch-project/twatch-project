9d36adc6933a7b2826f76fb491f2644866fb3a95
TAICHI01001 <anothai.0978452316@gmail.com>
2023-07-27 18:20:31 +0700

    CreateSlideComponent

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -1,8 +1,16 @@
 // import img3 from '../img/2.webp'
 import { Link } from 'react-router-dom';
+import Slide from '../components/Slide';
+const images = [
+  'https://images.unsplash.com/photo-1689852044062-8860ab627625?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  'https://plus.unsplash.com/premium_photo-1689596510033-37ba82784d7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1689969960265-230ed01d010e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1628808592686-345743b5aa9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+  'https://images.unsplash.com/photo-1690275176891-4718337fd788?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+];
 export default function Hero() {
-  const img3 =
-    'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80';
+  // const img3 =
+  //   'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80';
   return (
     <section className="flex justify-center items-start min-h-[100vh]">
       <div className="flex justify-center item-center gradient-sky h-[640px] ">
@@ -24,9 +32,10 @@ export default function Hero() {
             </div>
           </div>
 
-          <div className="flex justify-center items-center box-right ">
-            <div className="flex justify-center items-center  w-[510px] h-[300px]">
-              <img className="w-full h-full" src={img3} alt="" />
+          <div className="flex justify-center items-center box-right w-[50%] ">
+            <div className="flex justify-center items-center ">
+              {/* <img className="w-full h-full" src={img3} alt="" /> */}
+              <Slide images={images} slideInterval={3000} />
             </div>
           </div>
         </section>
