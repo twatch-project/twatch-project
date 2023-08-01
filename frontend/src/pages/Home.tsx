f1320db07c64ce6c2386537df793b88f9c45c9b2
rukchanok krumsamer <pepoil07@hotmail.com>
2023-08-01 20:43:29 +0700

    change design

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,10 @@
-// import { useAuth } from '../providers/AuthProvider'
-// import CompanyList from '../components/CompanyList'
-
 import HeroSection from '../components/HeroSection';
 import Sell from '../components/Sell';
 // import Blog from '../components/Blog';
 import Article from '../components/Article';
 import Footer from '../components/Footer';
 import CompanyList from '../components/companys/CompanyList';
-// import CompanyList from '../components/companys/CompanyList';
+
 const Home = () => {
   return (
     <>
