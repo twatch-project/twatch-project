d8e1a5cd5d8fffb4d76a46c0f7ca9004d95bb652
Chainarate <jn.chainarate@gmail.com>
2023-08-01 19:17:00 +0700

    add env VITE_BE_URL

--- frontend/src/constant/index.ts
+++ frontend/src/constant/index.ts
@@ -1,4 +1,8 @@
-export const host = 'http://localhost:8000';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+export const host = import.meta.env.VITE_BE_URL || 'http://localhost:8000'; // https://foo -> https://api.foo
 export const thaiProvinceData = 'https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_province.json';
 export const thaiAmphure = 'https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_amphure.json';
 export const Apitambons = 'https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_tambon.json';
